731edbd2783e5efe74a9f8042edfa49efcefa667
botue <botue@sina.com>
2022-09-07 19:04:47 +0800

    feat(house): 绑定房屋布局完成

---
+++ miniprogram/pages/house/index.ts
@@ -0,0 +1,19 @@
+import Dialog from '@vant/weapp/dialog/dialog'
+
+Page({
+  swipeClosed(ev) {
+    const { position, instance } = ev.detail
+
+    if (position === 'right') {
+      Dialog.confirm({
+        message: '是否删除已绑定房屋？',
+      })
+        .then(() => {
+          instance.close()
+        })
+        .catch(() => {
+          instance.close()
+        })
+    }
+  },
+})

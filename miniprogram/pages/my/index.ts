dd94b6b8acd490907c44b75807b851e0df318b0b
botue <botue@sina.com>
2022-09-30 18:00:32 +0800

    refactor: 合并分支样式

--- miniprogram/pages/my/index.ts
+++ miniprogram/pages/my/index.ts
@@ -1,1 +1,39 @@
-Page({})
+interface userInfo {
+  id: string
+  avatar: string
+  nickName: string
+}
+
+interface Data {
+  userInfo: userInfo
+}
+
+interface Method {
+  getUserInfo(): void
+}
+
+Page<Data, Method>({
+  onShow() {
+    // 获取登录用户信息
+    wx.getStorage({
+      key: 'token',
+      success: () => {
+        this.getUserInfo()
+      },
+    })
+  },
+
+  // 用户信息接口
+  async getUserInfo() {
+    // 请求数据接口
+    const { code, data: userInfo } = await wx.http.get<userInfo>('/userInfo')
+    // 校验数据是否合法
+    if (code !== 10000) return wx.showToast({ title: '数据加载失败, 请稍后重试!', icon: 'none' })
+    // 设置数据，更新渲染
+    this.setData({ userInfo })
+    // 本地缓存用户信息
+    wx.setStorage({ key: 'userInfo', data: userInfo })
+  },
+})
+
+export {}
--- miniprogram/pages/profile/index.ts
+++ miniprogram/pages/profile/index.ts
@@ -1,1 +1,24 @@
-Page({})
+Page({
+  data: {
+    userInfo: wx.getStorageSync('userInfo'),
+  },
+
+  getUserAvatar(ev: WechatMiniprogram.CustomEvent) {
+    this.updateUserInfo({ avatar: ev.detail.avatarUrl })
+  },
+
+  getUserNickName(ev: any) {
+    this.updateUserInfo({ nickName: ev.detail })
+  },
+
+  updateUserInfo(userInfo: { avatar?: string; nickName?: string }) {
+    wx.uploadFile({
+      url: 'https://live-api.itheima.net/userInfo',
+      name: 'avatar',
+      filePath: userInfo.avatar as string,
+      formData: { nickName: userInfo.nickName },
+    })
+  },
+})
+
+export {}
